77785cab108c02c41e4611a33b626803403ccbd7
Fabian Gerber <fabian@work>
2017-11-17 10:28:36 +0100

    pitcher extended

--- src/baseball/Pitcher.java
+++ src/baseball/Pitcher.java
@@ -1,8 +1,18 @@
 package baseball;
 
-public class Pitcher {
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class Pitcher implements EventHandler {
 	public Pitcher(Ball ball) {
-		
+
+	}
+
+	@Override
+	public void handle(Event arg0) {
+		// TODO Auto-generated method stub
+//		if(arg0.getSource() == ball)
+
 	}
 
 }
